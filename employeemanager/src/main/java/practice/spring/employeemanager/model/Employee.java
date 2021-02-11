ccebebb0080fd15eae76cea54e08a29e2f689eec
padmavathi gangasani <gangasanipadma@gmail.com>
2021-02-11 14:58:44 +0530

    Padma add model for employee

---
+++ employeemanager/src/main/java/practice/spring/employeemanager/model/Employee.java
@@ -0,0 +1,86 @@
+package practice.spring.employeemanager.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private long id;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phone;
+	private String imageURL;
+	@Column(nullable = false, updatable = false)
+	private String employeCode;
+	public Employee() {
+		
+	}
+	public Employee(long id, String name, String email, String jobTitle, String phone, String imageURL,
+			String employeCode) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.jobTitle = jobTitle;
+		this.phone = phone;
+		this.imageURL = imageURL;
+		this.employeCode = employeCode;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	public String getEmployeCode() {
+		return employeCode;
+	}
+	public void setEmployeCode(String employeCode) {
+		this.employeCode = employeCode;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", jobTitle=" + jobTitle + ", phone="
+				+ phone + ", imageURL=" + imageURL + ", employeCode=" + employeCode + "]";
+	}
+	
+	
+
+}
